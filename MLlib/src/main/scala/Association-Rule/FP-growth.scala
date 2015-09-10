b0d34f318c8b182999b453033de2226f9e51916d
anddelu <anddelu@users.noreply.github.com>
2015-09-10 16:26:44 +0800

    Update FP-growth.scala

--- MLlib/src/main/scala/Association-Rule/FP-growth.scala
+++ MLlib/src/main/scala/Association-Rule/FP-growth.scala
@@ -45,17 +45,17 @@ model.freqItemsets.collect().foreach { itemset =>
 }
 */
 
-
 val fpitems = 
 for (itemset <- model.freqItemsets.collect())
   yield ((itemset.items.mkString(",") + "," + itemset.freq + "," +BigDecimal(itemset.freq*1.0/totalExamples).setScale(3, BigDecimal.RoundingMode.HALF_UP).toDouble))
-  
 
 val fpitemsTrans = fpitems.map(_.split(","))
 val items = fpitemsTrans.map(a => a.toList.take(a.length-2))
 val Support = fpitemsTrans.map(a => a(a.length-1).toFloat)
+val Frequency = fpitemsTrans.map(a => a(a.length-2).toInt)
 val itemsAndSupport = items.zip(Support)
 val itemsMapSupport = items.zip(Support).toMap
+val itemsMapFrequency = items.zip(Frequency).toMap
 val minSupport = 0.60
 
 def isSafe(comA: List[String], comB: List[String]): Boolean = {
@@ -77,8 +77,6 @@ def showGenerListSupport(comStr: List[String]): Double = itemsMapSupport.get(com
 	 case None => 1.0
 	 }
 
-case class fpResult(comA: List[String], comAB: List[String], comB: List[String], confA2B: Float, supportB: Double, liftB: Double)
-
 val ruleCollect = 	 
 for {
      i <- itemsAndSupport
@@ -88,6 +86,7 @@ for {
 	 val gList = generList(i._1, j._1)
 	 val gListSupport = showGenerListSupport(gList)
 	 }
-yield fpResult(i._1, j._1, gList, j._2 / i._2, gListSupport, j._2 / (i._2 * gListSupport) )  
+yield fpResult(i._1, j._1, gList, itemsMapFrequency(j._1), j._2 / i._2, gListSupport, j._2 / (i._2 * gListSupport) )  
 
-ruleCollect.map(a => (a.comA, a.comB, a.comAB, a.confA2B, a.liftB)).foreach(println)
+ruleCollect.map(a => (a.comA, a.comB, a.comAB, a.frequencyAB, a.confA2B, a.supportB, a.liftB)).foreach(println)
+
